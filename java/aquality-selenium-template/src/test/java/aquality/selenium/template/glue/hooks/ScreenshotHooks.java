5468ba91fa109b00c64eef6685542a40e83f1221
Pavel Anihimovsky <pavel.anihimovsky@gmail.com>
2020-04-07 15:56:31 +0300

    Add demo test

---
+++ java/aquality-selenium-template/src/test/java/aquality/selenium/template/glue/hooks/ScreenshotHooks.java
@@ -0,0 +1,39 @@
+package aquality.selenium.template.glue.hooks;
+
+import aquality.selenium.browser.AqualityServices;
+import io.cucumber.core.api.Scenario;
+import io.cucumber.java.After;
+import ru.yandex.qatools.ashot.AShot;
+import ru.yandex.qatools.ashot.Screenshot;
+import ru.yandex.qatools.ashot.shooting.ShootingStrategies;
+import ru.yandex.qatools.ashot.shooting.ShootingStrategy;
+
+import javax.imageio.ImageIO;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+public class ScreenshotHooks {
+
+    @After(order = 1)
+    public void takeScreenshot(Scenario scenario) {
+        if (scenario.isFailed()) {
+            scenario.embed(attachScreenshot(), "image/png");
+        }
+    }
+
+    private byte[] attachScreenshot() {
+        int scrollTimeout = 500;
+        ShootingStrategy shootingStrategy = ShootingStrategies.viewportPasting(scrollTimeout);
+        Screenshot fpScreenshot = new AShot()
+                .shootingStrategy(shootingStrategy)
+                .takeScreenshot(AqualityServices.getBrowser().getDriver());
+        try (ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
+            ImageIO.write(fpScreenshot.getImage(), "jpg", baos);
+            baos.flush();
+            return baos.toByteArray();
+        } catch (IOException ioe) {
+            AqualityServices.getLogger().warn(String.format("IO Exception during preparing screenshot of full page%nException message: %s", ioe.getMessage()));
+            return new byte[] {};
+        }
+    }
+}
