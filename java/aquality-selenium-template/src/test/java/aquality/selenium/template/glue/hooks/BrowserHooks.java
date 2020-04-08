059f8d2f64a3b80238a44bb40b5508edeee43ecc
Pavel Anihimovsky <pavel.anihimovsky@gmail.com>
2020-04-08 10:41:15 +0300

    Remove Browser start hook

--- java/aquality-selenium-template/src/test/java/aquality/selenium/template/glue/hooks/BrowserHooks.java
+++ java/aquality-selenium-template/src/test/java/aquality/selenium/template/glue/hooks/BrowserHooks.java
@@ -2,14 +2,8 @@ package aquality.selenium.template.glue.hooks;
 
 import aquality.selenium.browser.AqualityServices;
 import io.cucumber.java.After;
-import io.cucumber.java.Before;
 
 public class BrowserHooks {
-
-    @Before
-    public void startBrowser() {
-        AqualityServices.getBrowser();
-    }
 
     @After(order = 0)
     public void closeBrowser() {
