b40d4ba4de61163361348d9713e255a1ef0d9604
Akzhol Aidossuly <aidosulyakzhol@gmail.com>
2025-07-30 17:35:01 +0500

    newbranch13

--- aquality-selenium-template-testng/src/test/java/aquality/selenium/template/testng/tests/BaseTest.java
+++ aquality-selenium-template-testng/src/test/java/aquality/selenium/template/testng/tests/BaseTest.java
@@ -47,7 +47,7 @@ public class BaseTest {
         browser.waitForPageToLoad();
     }
 
-    @AfterMethod
+    @AfterMetho
     public void cleanUp(ITestContext context) {
         if (AqualityServices.isBrowserStarted()) {
             Allure.addAttachment("page source", "text/html",
@@ -58,3 +58,11 @@ public class BaseTest {
         }
     }
 }
+
+
+
+
+
+
+
+//adding teemfgqorugnoer
